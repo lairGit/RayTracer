1e049dc9510310ee15eb2c70ddbd30ee085ece98
Jack Lair <jacklair1993@gmail.com>
2021-07-06 17:51:30 -0700

    Set up project; Added ray, vec3, and main

---
+++ RayTracer/Program.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "vec3.h"
+
+int main()
+{
+	int nx = 200;
+	int ny = 100;
+	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+	for (int j = ny - 1; j >= 0; j--)
+	{
+		for (int i = 0; i < nx; i++)
+		{
+			vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2f);
+
+			int ir = int(255.99 * col.r());
+			int ig = int(255.99 * col.g());
+			int ib = int(255.99 * col.b());
+			std::cout << ir << " " << ig << " " << ib << "\n";
+		}
+	}
+}
